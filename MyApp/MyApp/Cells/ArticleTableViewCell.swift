8a8e140ea4817692742113f0f3b7b7ed00a03547
jun.kohda <>
2022-09-17 18:03:37 +0900

    お手本通り実装

---
+++ MyApp/MyApp/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  MyApp
+//
+//  Created by jun.kohda on 2022/09/17.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
